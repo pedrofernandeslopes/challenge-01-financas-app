281c4ff08e426093d23a2aed844de0361f3c035a
pedro <pedroflopes1710@gmail.com>
2023-09-29 11:59:49 -0300

    feat: added actions component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, FlatListProps  } from 'react-native';
 import Header, { } from '../../components/Header';
 import Balance from '../../components/Balance';
 import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
 
 
 const list = [
@@ -37,6 +38,8 @@ export default function Home() {
 
             <Balance saldo='9.250,90' gastos='-527,00' />
 
+            <Actions />
+
             <Text style={styles.title}>Últimas movimentações</Text>
 
             <FlatList 
@@ -59,9 +62,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 18,
         fontWeight: 'bold',
-        marginLeft: 14,
-        marginRight: 14,
-        marginTop: 14,
+        margin: 14,
     },
     list: {
         marginStart: 14,
