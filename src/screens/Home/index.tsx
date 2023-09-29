144c06a5b6d02ed3fa32d4389a97ea7e858ebef0
pedro <pedroflopes1710@gmail.com>
2023-09-29 11:44:04 -0300

    feat: add last movements components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, FlatListProps  } from 'react-native';
+
 import Header, { } from '../../components/Header';
 import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
 
 
 const list = [
@@ -8,21 +10,21 @@ const list = [
         id: 1,
         label: 'Boleto conta luz',
         value: '300,90',
-        date: '10/10/2020',
+        date: '20/09/2023',
         type: 0, // despesas
     },
     {
         id: 2,
         label: 'Pix cliente X',
         value: '2.500,00',
-        date: '10/10/2020',
+        date: '22/09/2023',
         type: 1, // receitas
     },
     {
         id: 3,
         label: 'Salário',
         value: '7.500,00',
-        date: '10/10/2020',
+        date: '10/09/2023',
         type: 1, // receitas
     },
 ]
@@ -36,6 +38,14 @@ export default function Home() {
             <Balance saldo='9.250,90' gastos='-527,00' />
 
             <Text style={styles.title}>Últimas movimentações</Text>
+
+            <FlatList 
+                style={styles.list}
+                data={list}
+                keyExtractor={ (item) => String(item.id) }
+                showsVerticalScrollIndicator={false}
+                renderItem={ ({ item }) => <Movements data={item} /> }
+            />
 
         </View>
     );
@@ -52,5 +62,9 @@ const styles = StyleSheet.create({
         marginLeft: 14,
         marginRight: 14,
         marginTop: 14,
-    }
+    },
+    list: {
+        marginStart: 14,
+        marginEnd: 14,
+    },
 })
