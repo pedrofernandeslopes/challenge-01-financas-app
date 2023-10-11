b0d72aadc488472c9434c8e2d48d1be6723f84fd
pedro <pedroflopes1710@gmail.com>
2023-10-10 23:23:45 -0300

    home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -49,7 +49,6 @@ export default function Home() {
                 showsVerticalScrollIndicator={false}
                 renderItem={ ({ item }) => <Movements data={item} /> }
             />
-
         </View>
     );
 }
